81544e08661e9db58008f216e0ef3b86d0aa47b8
DESKTOP-GIRJ689\dell <dvazquez@crediespacios.com>
2022-05-06 17:43:22 -0500

    Creamos los controladores para ver la pagina principal index, para crerar con create y para ver algo en principal con show, por otro lado agregamos a las rutas los controlodares con suj respectivo metodo

---
+++ app/Http/Controllers/CursoController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CursoController extends Controller
+{
+    public function index()
+    {
+        return "Hola bienvneido a la pagina principal";
+    }
+
+    public function create()
+    {
+        return "En esta pagina podras crear un curso";
+    }
+
+    public function show($curso)
+    {
+        return "Bienvenido al curso: $curso";
+    }
+}
